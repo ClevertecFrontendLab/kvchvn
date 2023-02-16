212a66cec57c8508c9f98e8a8b5f203198aa75b6
Anton Kachan <aa.kvchvn@gmail.com>
2023-02-17 00:34:55 +0300

    feat: insert Toast in MainPage, BookPage

--- src/pages/book/BookPage.tsx
+++ src/pages/book/BookPage.tsx
@@ -4,39 +4,46 @@ import { BookMainContent } from '../../components/book-page/book-main-content';
 import { BookSubContent } from '../../components/book-page/book-sub-content';
 import { Breadcrumbs } from '../../components/common/breadcrumbs';
 import { Loading } from '../../components/global/loading';
+import { Toast } from '../../components/global/toast';
 import { Wrapper } from '../../components/global/wrapper';
 import { MainNavigation } from '../../components/navigation/main-navigation';
-import { useCategoriesSelector, useGetBookByIdQuery } from '../../store';
+import { DEFAULT_ERROR_MESSAGE, QUERY_STATUS } from '../../constants';
+import { useCategoriesSelector, useCategoriesStatusSelector, useGetBookByIdQuery } from '../../store';
 
 import styles from './BookPage.module.scss';
 
 export const BookPage = () => {
   const { category, bookId } = useParams();
   const categories = useCategoriesSelector();
-  const { isLoading, isError, data: book } = useGetBookByIdQuery(bookId as string);
+  const categoriesStatus = useCategoriesStatusSelector();
+  const { isLoading: isBookLoading, isError: isBookError, data: book } = useGetBookByIdQuery(bookId as string);
 
   return (
     <main className={styles.main}>
-      {isLoading && <Loading />}
-      {isError && <h1>Error</h1>}
+      {(isBookLoading || categoriesStatus === QUERY_STATUS.isLoading) && <Loading />}
+      {(isBookError || categoriesStatus === QUERY_STATUS.isError) && (
+        <Toast type='error' message={DEFAULT_ERROR_MESSAGE} />
+      )}
       {categories && (
         <Breadcrumbs paths={[categories[categories.findIndex((cat) => cat.path === category)].name, book?.title]} />
       )}
-      {book && (
-        <Wrapper>
-          <MainNavigation />
-          <BookMainContent
-            title={book.title}
-            authors={book.authors}
-            issueYear={book.details.issueYear}
-            description={book.description}
-            posterPaths={book.images}
-            delivery={book.delivery}
-            booking={book.booking}
-          />
-          <BookSubContent rating={book.rating} details={book.details} comments={book.comments} />
-        </Wrapper>
-      )}
+      <Wrapper>
+        <MainNavigation />
+        {book && (
+          <>
+            <BookMainContent
+              title={book.title}
+              authors={book.authors}
+              issueYear={book.details.issueYear}
+              description={book.description}
+              posterPaths={book.images}
+              delivery={book.delivery}
+              booking={book.booking}
+            />
+            <BookSubContent rating={book.rating} details={book.details} comments={book.comments} />
+          </>
+        )}
+      </Wrapper>
     </main>
   );
 };
--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -3,10 +3,11 @@ import { useParams } from 'react-router-dom';
 
 import { NotFoundView } from '../../components/common/not-found-view';
 import { Loading } from '../../components/global/loading';
+import { Toast } from '../../components/global/toast';
 import { Wrapper } from '../../components/global/wrapper';
 import { Books } from '../../components/main-page/books';
 import { MainNavigation } from '../../components/navigation/main-navigation';
-import { QUERY_STATUS } from '../../constants';
+import { DEFAULT_ERROR_MESSAGE, QUERY_STATUS } from '../../constants';
 import { PARAMS } from '../../constants/common';
 import { useCategoriesSelector, useCategoriesStatusSelector, useGetAllBooksQuery } from '../../store';
 
@@ -26,7 +27,9 @@ export const MainPage = () => {
   return (
     <main className={styles.main}>
       {(isAllBooksLoading || categoriesStatus === QUERY_STATUS.isLoading) && <Loading />}
-      {(isAllBooksError || categoriesStatus === QUERY_STATUS.isError) && <h1>Error</h1>}
+      {(isAllBooksError || categoriesStatus === QUERY_STATUS.isError) && (
+        <Toast type='error' message={DEFAULT_ERROR_MESSAGE} />
+      )}
       {isUnknownCategory ? (
         <NotFoundView />
       ) : (
