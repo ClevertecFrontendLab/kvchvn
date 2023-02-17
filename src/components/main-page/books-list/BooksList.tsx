00a00b61680af2daee527bc65de61e78895af368
Anton Kachan <aa.kvchvn@gmail.com>
2023-02-17 20:59:10 +0300

    refactor: stand out filteredAndSortedBooks

--- src/components/main-page/books-list/BooksList.tsx
+++ src/components/main-page/books-list/BooksList.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { ALL_BOOKS_CATEGORY, MAX_RATING } from '../../../constants';
+import { ALL_BOOKS_CATEGORY, SORT } from '../../../constants';
 import { PARAMS } from '../../../constants/common';
 import { useAllBooksSelector, useCategoriesSelector, useSortingByRatingSelector } from '../../../store';
 import { BooksView } from '../../../types';
@@ -20,36 +20,45 @@ export const BooksList = ({ booksView }: BooksListProps) => {
   const categories = useCategoriesSelector();
   const sortingByRating = useSortingByRatingSelector();
 
-  return books ? (
+  const filteredAndSortedBooks = useMemo(
+    () =>
+      books
+        ? books
+            .filter((book) => {
+              if (book.categories && categories && categoryPath && categoryPath !== ALL_BOOKS_CATEGORY.path) {
+                const currentCategory = categories.find((cat) => cat.path === categoryPath);
+
+                if (currentCategory && !book.categories.includes(currentCategory.name)) {
+                  return false;
+                }
+              }
+
+              return true;
+            })
+            .sort((bookA, bookB) => {
+              const DEFAULT_RATING = 0;
+              const bookARating = bookA.rating || DEFAULT_RATING;
+              const bookBRating = bookB.rating || DEFAULT_RATING;
+
+              switch (sortingByRating) {
+                case SORT.asc:
+                  return bookARating - bookBRating;
+                case SORT.desc:
+                default:
+                  return bookBRating - bookARating;
+              }
+            })
+        : undefined,
+    [books, categories, categoryPath, sortingByRating]
+  );
+
+  return filteredAndSortedBooks ? (
     <ul className={`${styles['books-list']} ${styles[`${booksView}-view`]}`}>
-      {books
-        .filter((book) => {
-          if (book.categories && categories && categoryPath && categoryPath !== ALL_BOOKS_CATEGORY.path) {
-            const currentCategory = categories.find((cat) => cat.path === categoryPath);
-
-            if (currentCategory && !book.categories.includes(currentCategory.name)) {
-              return false;
-            }
-          }
-
-          return true;
-        })
-        .sort((bookA, bookB) => {
-          const DEFAULT_RATING = sortingByRating === 'desc' ? MAX_RATING + 1 : 0;
-          const bookARating = bookA.rating || DEFAULT_RATING;
-          const bookBRating = bookB.rating || DEFAULT_RATING;
-
-          switch (sortingByRating) {
-            case 'asc':
-              return bookBRating - bookARating;
-            case 'desc':
-            default:
-              return bookARating - bookBRating;
-          }
-        })
-        .map((book) => (
-          <BookCard key={book.id} view={booksView} book={book} />
-        ))}
+      {filteredAndSortedBooks.length ? (
+        filteredAndSortedBooks.map((book) => <BookCard key={book.id} view={booksView} book={book} />)
+      ) : (
+        <h3>В этой категории книг еще нет</h3>
+      )}
     </ul>
   ) : null;
 };
