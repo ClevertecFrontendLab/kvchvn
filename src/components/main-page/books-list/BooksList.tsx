048abbe69ecdbaafc73decbc96bc858e3628478e
Anton Kachan <aa.kvchvn@gmail.com>
2023-02-17 15:58:56 +0300

    feat: add sorting by categories in BookList

--- src/components/main-page/books-list/BooksList.tsx
+++ src/components/main-page/books-list/BooksList.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
-import { useAllBooksSelector } from '../../../store';
+import { ALL_BOOKS_CATEGORY } from '../../../constants';
+import { PARAMS } from '../../../constants/common';
+import { useAllBooksSelector, useCategoriesSelector } from '../../../store';
 import { BooksView } from '../../../types';
 import { BookCard } from '../book-card';
 
@@ -11,13 +14,23 @@ interface BooksListProps {
 }
 
 export const BooksList = ({ booksView }: BooksListProps) => {
+  const { category: categoryPath } = useParams<keyof typeof PARAMS>();
   const books = useAllBooksSelector();
+  const categories = useCategoriesSelector();
 
   return books ? (
     <ul className={`${styles['books-list']} ${styles[`${booksView}-view`]}`}>
-      {books.map((book) => (
-        <BookCard key={book.id} view={booksView} book={book} />
-      ))}
+      {books.map((book) => {
+        if (categories && categoryPath && categoryPath !== ALL_BOOKS_CATEGORY.path) {
+          const currentCategory = categories.find((cat) => cat.path === categoryPath);
+
+          if (currentCategory && book.categories && !book.categories.includes(currentCategory.name)) {
+            return null;
+          }
+        }
+
+        return <BookCard key={book.id} view={booksView} book={book} />;
+      })}
     </ul>
   ) : null;
 };
