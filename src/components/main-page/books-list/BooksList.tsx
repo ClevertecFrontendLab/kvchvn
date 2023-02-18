767063d747cb58f681b4fe7d7561e3feca675edf
Anton Kachan <aa.kvchvn@gmail.com>
2023-02-18 13:38:17 +0300

    fix: remove useMemo from BookList as unnecessary

--- src/components/main-page/books-list/BooksList.tsx
+++ src/components/main-page/books-list/BooksList.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 
 import { ALL_BOOKS_CATEGORY, SORT } from '../../../constants';
@@ -14,43 +14,39 @@ interface BooksListProps {
 }
 
 export const BooksList = ({ booksView }: BooksListProps) => {
-  const { category: categoryPath } = useParams<keyof typeof PARAMS>();
+  const { category: currentCategoryPath } = useParams<keyof typeof PARAMS>();
 
   const books = useAllBooksSelector();
   const categories = useCategoriesSelector();
   const sortingByRating = useSortingByRatingSelector();
 
-  const filteredAndSortedBooks = useMemo(
-    () =>
-      books
-        ? books
-            .filter((book) => {
-              if (book.categories && categories && categoryPath && categoryPath !== ALL_BOOKS_CATEGORY.path) {
-                const currentCategory = categories.find((cat) => cat.path === categoryPath);
+  const filteredAndSortedBooks = books
+    ? books
+        .filter((book) => {
+          if (book.categories && categories && currentCategoryPath && currentCategoryPath !== ALL_BOOKS_CATEGORY.path) {
+            const currentCategory = categories.find((cat) => cat.path === currentCategoryPath);
 
-                if (currentCategory && !book.categories.includes(currentCategory.name)) {
-                  return false;
-                }
-              }
+            if (currentCategory && !book.categories.includes(currentCategory.name)) {
+              return false;
+            }
+          }
 
-              return true;
-            })
-            .sort((bookA, bookB) => {
-              const DEFAULT_RATING = 0;
-              const bookARating = bookA.rating || DEFAULT_RATING;
-              const bookBRating = bookB.rating || DEFAULT_RATING;
+          return true;
+        })
+        .sort((bookA, bookB) => {
+          const DEFAULT_RATING = 0;
+          const bookARating = bookA.rating || DEFAULT_RATING;
+          const bookBRating = bookB.rating || DEFAULT_RATING;
 
-              switch (sortingByRating) {
-                case SORT.asc:
-                  return bookARating - bookBRating;
-                case SORT.desc:
-                default:
-                  return bookBRating - bookARating;
-              }
-            })
-        : undefined,
-    [books, categories, categoryPath, sortingByRating]
-  );
+          switch (sortingByRating) {
+            case SORT.asc:
+              return bookARating - bookBRating;
+            case SORT.desc:
+            default:
+              return bookBRating - bookARating;
+          }
+        })
+    : undefined;
 
   return filteredAndSortedBooks ? (
     <ul className={`${styles['books-list']} ${styles[`${booksView}-view`]}`}>
