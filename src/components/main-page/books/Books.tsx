edcca9e8ed1037c5a82b5adb10b551f190689122
Anton Kachan <aa.kvchvn@gmail.com>
2023-02-19 21:21:33 +0300

    refactor: simplify Books component

--- src/components/main-page/books/Books.tsx
+++ src/components/main-page/books/Books.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
 
-import { useBooksViewSelector } from '../../../store';
 import { BooksNavigation } from '../../navigation/books-navigation';
 import { BooksList } from '../books-list';
 
 import styles from './Books.module.scss';
 
-export const Books = () => {
-  const booksView = useBooksViewSelector();
-
-  return (
-    <section className={styles.section}>
-      <BooksNavigation booksView={booksView} />
-      <BooksList booksView={booksView} />
-    </section>
-  );
-};
+export const Books = () => (
+  <section className={styles.section}>
+    <BooksNavigation />
+    <BooksList />
+  </section>
+);
