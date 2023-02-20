f12bb77d98714df56d91ced46f53a642cc7c90c0
Anton Kachan <aa.kvchvn@gmail.com>
2023-02-20 12:20:56 +0300

    feat: allocate SearchBox with its logic and styles from BooksNavigation; use useDebounce

--- src/components/navigation/books-navigation/BooksNavigation.tsx
+++ src/components/navigation/books-navigation/BooksNavigation.tsx
@@ -3,14 +3,13 @@ import classnames from 'classnames';
 
 import { BOOKS_LIST_VIEW, BOOKS_TABLE_VIEW, INITIAL_SORT_BY_RATING } from '../../../constants';
 import {
-  setSearchValue,
   toggleBooksView,
   toggleSortingByRating,
   useBooksViewSelector,
-  useSearchValueSelector,
   useSortingByRatingSelector,
 } from '../../../store';
 import { useAppDispatch } from '../../../store/store';
+import { SearchBox } from '../../book-page/search-box';
 
 import styles from './BooksNavigation.module.scss';
 
@@ -18,21 +17,13 @@ export const BooksNavigation = () => {
   const [isExpandedSearchBox, setIsExpandedSearchBox] = useState(false);
 
   const booksView = useBooksViewSelector();
-  const searchValue = useSearchValueSelector();
   const sortingByRating = useSortingByRatingSelector();
   const dispatch = useAppDispatch();
 
   const complexStyles = {
-    searchBox: classnames(styles['search-box'], { [styles.expanded]: isExpandedSearchBox }),
     sortBox: classnames(styles['sort-box'], { [styles.hidden]: isExpandedSearchBox }),
     buttonsBox: classnames(styles['view-buttons-box'], { [styles.hidden]: isExpandedSearchBox }),
   };
-
-  const handleClick = () => setIsExpandedSearchBox(true);
-
-  const hideSearchBox = () => setIsExpandedSearchBox(false);
-
-  const handleChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => dispatch(setSearchValue(e.target.value));
 
   const handleChangeSorting = () => dispatch(toggleSortingByRating());
 
@@ -40,18 +31,7 @@ export const BooksNavigation = () => {
 
   return (
     <nav className={styles.nav}>
-      <label htmlFor='search' className={complexStyles.searchBox} data-test-id='button-search-open'>
-        <input
-          type='search'
-          id='search'
-          placeholder='Поиск книги или автора ...'
-          onClick={handleClick}
-          value={searchValue}
-          onChange={handleChangeSearch}
-          data-test-id='input-search'
-        />
-        <button type='button' onClick={hideSearchBox} data-test-id='button-search-close' />
-      </label>
+      <SearchBox isExpanded={isExpandedSearchBox} setIsExpanded={setIsExpandedSearchBox} />
       <label htmlFor='sort' className={complexStyles.sortBox}>
         По рейтингу
         <input
