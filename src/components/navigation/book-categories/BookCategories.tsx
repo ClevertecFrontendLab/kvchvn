1af40c8c827370861acd237671b27ccecb0d467c
Anton Kachan <aa.kvchvn@gmail.com>
2023-02-20 20:32:41 +0300

    fix: fix BookCategories

--- src/components/navigation/book-categories/BookCategories.tsx
+++ src/components/navigation/book-categories/BookCategories.tsx
@@ -4,12 +4,7 @@ import classnames from 'classnames';
 
 import { ALL_BOOKS_CATEGORY, ROUTES, SCREEN_SIZE_OF_MENU_TOGGLE_VISIBILITY } from '../../../constants';
 import { calculateBooksByCategories } from '../../../helpers';
-import {
-  toggleBookCategoriesVisibility,
-  useAllBooksSelector,
-  useBookCategoriesVisibilitySelector,
-} from '../../../store';
-import { useAppDispatch } from '../../../store/store';
+import { useAllBooksSelector, useBookCategoriesVisibilitySelector } from '../../../store';
 import { Category } from '../../../types';
 
 import styles from './BooksCategories.module.scss';
@@ -21,15 +16,12 @@ interface BookCategoriesProps {
 export const BookCategories = ({ categories }: BookCategoriesProps) => {
   const isBookCategoriesVisible = useBookCategoriesVisibilitySelector();
   const allBooks = useAllBooksSelector();
-  const dispatch = useAppDispatch();
 
   const complexStyles = {
     list: classnames(styles['category-list'], { [styles.hidden]: !isBookCategoriesVisible }),
   };
 
   const categoriesWithAmount = calculateBooksByCategories(categories, allBooks);
-
-  const hideCategories = () => dispatch(toggleBookCategoriesVisibility());
 
   return (
     <ul className={complexStyles.list}>
@@ -38,7 +30,6 @@ export const BookCategories = ({ categories }: BookCategoriesProps) => {
           <NavLink
             to={`${ROUTES.books.base}/${category.path}`}
             className={({ isActive }) => (isActive ? styles.active : undefined)}
-            onClick={hideCategories}
             data-test-id={
               window.innerWidth > SCREEN_SIZE_OF_MENU_TOGGLE_VISIBILITY
                 ? `navigation-${category.path === ALL_BOOKS_CATEGORY.path ? 'books' : category.path}`
@@ -46,16 +37,16 @@ export const BookCategories = ({ categories }: BookCategoriesProps) => {
             }
           >
             {category.name}
-            <span
-              data-test-id={
-                window.innerWidth > SCREEN_SIZE_OF_MENU_TOGGLE_VISIBILITY
-                  ? `navigation-book-count-for-${category.path === ALL_BOOKS_CATEGORY.path ? 'books' : category.path}`
-                  : `burger-book-count-for-${category.path === ALL_BOOKS_CATEGORY.path ? 'books' : category.path}`
-              }
-            >
-              {category.path === ALL_BOOKS_CATEGORY.path || !('amount' in category) ? '' : category.amount}
-            </span>
           </NavLink>
+          <span
+            data-test-id={
+              window.innerWidth > SCREEN_SIZE_OF_MENU_TOGGLE_VISIBILITY
+                ? `navigation-book-count-for-${category.path === ALL_BOOKS_CATEGORY.path ? 'books' : category.path}`
+                : `burger-book-count-for-${category.path === ALL_BOOKS_CATEGORY.path ? 'books' : category.path}`
+            }
+          >
+            {category.path === ALL_BOOKS_CATEGORY.path || !('amount' in category) ? '' : category.amount}
+          </span>
         </li>
       ))}
     </ul>
