52df0ffa37dd5f01c15f0bacad38f1ed6608e515
Anton Kachan <aa.kvchvn@gmail.com>
2023-03-01 14:26:39 +0300

    feat: create types connected with authorization

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,3 +1,5 @@
+import { Nullable } from './common';
+
 export type RegistrationInputName = 'login' | 'password' | 'firstName' | 'lastName' | 'phone' | 'email';
 
 export type InputContent<T extends string> = {
@@ -9,3 +11,42 @@ export type InputContent<T extends string> = {
 };
 
 export type RegistrationSteps = JSX.Element[][];
+
+interface User {
+  id: Nullable<number>;
+  username: Nullable<string>;
+  email: Nullable<string>;
+  provider: Nullable<string>;
+  confirmed: Nullable<boolean>;
+  blocked: Nullable<boolean>;
+  createdAt: Nullable<string>;
+  updatedAt: Nullable<string>;
+  firstName: Nullable<string>;
+  lastName: Nullable<string>;
+  phone: Nullable<string>;
+}
+
+export interface RegistrationRequestBody {
+  email: string;
+  username: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  phone: string;
+}
+
+export interface AuthResponse {
+  jwt: string;
+  user: Nullable<User>;
+}
+
+export interface AuthRequestBody {
+  identifier: string;
+  password: string;
+}
+
+export interface PasswordResettingRequestBody {
+  password: string;
+  passwordConfirmation: string;
+  code: string;
+}
