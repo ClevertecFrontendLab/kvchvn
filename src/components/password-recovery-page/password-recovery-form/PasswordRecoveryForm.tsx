2782a0a160da95187518566beccd599e61fb85df
Anton Kachan <aa.kvchvn@gmail.com>
2023-03-01 12:01:59 +0300

    feat: create PasswordRecoveryPage with its own route

---
+++ src/components/password-recovery-page/password-recovery-form/PasswordRecoveryForm.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { PASSWORD_RECOVERY_INPUT } from '../../../constants';
+import { InputBox } from '../../common/input-box';
+
+export const PasswordRecoveryForm = () => {
+  const handleClick = () => alert('Восстановление');
+
+  return (
+    <>
+      <div />
+      <h4>Восстановление пароля</h4>
+      <InputBox
+        type='email'
+        name={PASSWORD_RECOVERY_INPUT.email.name}
+        label={PASSWORD_RECOVERY_INPUT.email.label}
+        isError={false}
+        assistiveText={PASSWORD_RECOVERY_INPUT.email.assistiveText}
+      />
+    </>
+  );
+};
