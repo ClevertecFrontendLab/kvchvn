06db8d9c6fdbc6eb851595b55de791014f7e924d
Anton Kachan <aa.kvchvn@gmail.com>
2023-03-01 12:22:01 +0300

    feat: add submit-box in PasswordRecoveryPage

--- src/components/password-recovery-page/password-recovery-form/PasswordRecoveryForm.tsx
+++ src/components/password-recovery-page/password-recovery-form/PasswordRecoveryForm.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import { PASSWORD_RECOVERY_INPUT } from '../../../constants';
+import { PASSWORD_RECOVERY_INPUT, ROUTES } from '../../../constants';
 import { InputBox } from '../../common/input-box';
+
+import styles from './PasswordRecoveryForm.module.scss';
 
 export const PasswordRecoveryForm = () => {
   const handleClick = () => alert('Восстановление');
@@ -10,12 +13,23 @@ export const PasswordRecoveryForm = () => {
     <>
       <div />
       <h4>Восстановление пароля</h4>
-      <InputBox
-        type='email'
-        name={PASSWORD_RECOVERY_INPUT.email.name}
-        label={PASSWORD_RECOVERY_INPUT.email.label}
-        assistiveText={PASSWORD_RECOVERY_INPUT.email.assistiveText}
-      />
+      <form autoComplete='off'>
+        <InputBox
+          type='email'
+          name={PASSWORD_RECOVERY_INPUT.email.name}
+          label={PASSWORD_RECOVERY_INPUT.email.label}
+          assistiveText={PASSWORD_RECOVERY_INPUT.email.assistiveText}
+        />
+      </form>
+      <div className={styles['submit-box']}>
+        <button type='button' onClick={handleClick}>
+          Восстановить
+        </button>
+        <p>
+          Нет учетной записи?
+          <Link to={ROUTES.registration}>Регистрация</Link>
+        </p>
+      </div>
     </>
   );
 };
