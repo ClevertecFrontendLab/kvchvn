22a5b51c6ed8c929f5b0637dafc9c4fb2085c0d3
Anton Kachan <aa.kvchvn@gmail.com>
2023-03-02 11:04:00 +0300

    feat: break down RegistrationSteps into smaller components

--- src/components/registration-page/registration-steps/RegistrationSteps.tsx
+++ src/components/registration-page/registration-steps/RegistrationSteps.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import classnames from 'classnames';
 
-import { REGISTRATION_FIRST_STEP, REGISTRATION_INPUT, REGISTRATION_LAST_STEP } from '../../../constants';
-import { InputBox } from '../../common/input-box';
+import { REGISTRATION_FIRST_STEP, REGISTRATION_LAST_STEP } from '../../../constants';
+import { FirstStep } from '../first-step';
+import { SecondStep } from '../second-step';
+import { ThirdStep } from '../third-step';
 
 import styles from './RegistrationSteps.module.scss';
 
@@ -11,42 +12,11 @@ interface SignUpStepsProps {
 }
 
 export const RegistrationSteps = ({ currentStep }: SignUpStepsProps) => {
-  const complexStyles = {
-    firstStepItem: classnames(styles.item, { [styles.hidden]: currentStep !== REGISTRATION_FIRST_STEP }),
-    secondStepItem: classnames(styles.item, { [styles.hidden]: currentStep !== REGISTRATION_FIRST_STEP + 1 }),
-    lastStepItem: classnames(styles.item, { [styles.hidden]: currentStep !== REGISTRATION_LAST_STEP }),
-  };
+  const steps = [
+    { step: REGISTRATION_FIRST_STEP, component: <FirstStep /> },
+    { step: REGISTRATION_FIRST_STEP + 1, component: <SecondStep /> },
+    { step: REGISTRATION_LAST_STEP, component: <ThirdStep /> },
+  ];
 
-  return (
-    <ul className={styles['inputs-list']}>
-      <li className={complexStyles.firstStepItem}>
-        <InputBox
-          type='text'
-          name={REGISTRATION_INPUT.login.name}
-          label={REGISTRATION_INPUT.login.label}
-          assistiveText={REGISTRATION_INPUT.login.assistiveText}
-        />
-      </li>
-      <li className={complexStyles.firstStepItem}>
-        <InputBox
-          type='password'
-          name={REGISTRATION_INPUT.password.name}
-          label={REGISTRATION_INPUT.password.label}
-          assistiveText={REGISTRATION_INPUT.password.assistiveText}
-        />
-      </li>
-      <li className={complexStyles.secondStepItem}>
-        <InputBox type='text' name={REGISTRATION_INPUT.firstName.name} label={REGISTRATION_INPUT.firstName.label} />
-      </li>
-      <li className={complexStyles.secondStepItem}>
-        <InputBox type='text' name={REGISTRATION_INPUT.lastName.name} label={REGISTRATION_INPUT.lastName.label} />
-      </li>
-      <li className={complexStyles.lastStepItem}>
-        <InputBox type='text' name={REGISTRATION_INPUT.phone.name} label={REGISTRATION_INPUT.phone.label} />
-      </li>
-      <li className={complexStyles.lastStepItem}>
-        <InputBox type='email' name={REGISTRATION_INPUT.email.name} label={REGISTRATION_INPUT.email.label} />
-      </li>
-    </ul>
-  );
+  return <ul className={styles['inputs-list']}>{steps.find(({ step }) => step === currentStep)?.component || null}</ul>;
 };
