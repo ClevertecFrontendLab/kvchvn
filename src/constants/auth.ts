1cb76e91029f7fb620c4c324534f8bf0693ccd47
Anton Kachan <aa.kvchvn@gmail.com>
2023-03-05 23:17:05 +0300

    feat: add constants related to validations

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,5 +1,7 @@
 import { InputContent, RegistrationInputName } from '../types';
 
+import { LOGIN_VALIDATION_SUBJECT, PASSWORD_VALIDATION_SUBJECT } from './validation';
+
 export const REGISTRATION_FIRST_STEP = 1;
 export const REGISTRATION_LAST_STEP = 3;
 
@@ -7,12 +9,12 @@ export const REGISTRATION_INPUT: InputContent<RegistrationInputName> = {
   login: {
     name: 'login',
     label: 'Придумайте логин для входа',
-    assistiveText: 'Используйте для логина латинский алфавит и цифры',
+    assistiveText: `Используйте для логина ${LOGIN_VALIDATION_SUBJECT.latinLetter} и ${LOGIN_VALIDATION_SUBJECT.number}`,
   },
   password: {
     name: 'password',
     label: 'Пароль',
-    assistiveText: 'Пароль не менее 8 символов, с заглавной буквой и цифрой',
+    assistiveText: `Пароль ${PASSWORD_VALIDATION_SUBJECT.length}, с ${PASSWORD_VALIDATION_SUBJECT.capitalLetter} и ${PASSWORD_VALIDATION_SUBJECT.number}`,
   },
   firstName: {
     name: 'firstName',
@@ -50,3 +52,5 @@ export const PASSWORD_RECOVERY_INPUT: InputContent<'email'> = {
     assistiveText: 'На это email будет отправлено письмо с инструкциями по восстановлению пароля',
   },
 };
+
+export const REQUIRED_FIELD_ERROR = 'Поле не может быть пустым';
