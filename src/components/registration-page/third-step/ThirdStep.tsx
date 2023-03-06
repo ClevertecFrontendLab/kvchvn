56ed946fa7796ce1f01ef3a79b7198e071565148
Anton Kachan <aa.kvchvn@gmail.com>
2023-03-07 01:01:11 +0300

    fix: fix props in ThirdStep

--- src/components/registration-page/third-step/ThirdStep.tsx
+++ src/components/registration-page/third-step/ThirdStep.tsx
@@ -1,30 +1,25 @@
 import React from 'react';
-import { useFormContext } from 'react-hook-form';
 
 import { REGISTRATION_INPUT } from '../../../constants';
 import { InputBox } from '../../common/input-box';
 
-export const ThirdStep = () => {
-  const { register } = useFormContext();
-
-  return (
-    <>
-      <li>
-        <InputBox
-          type='text'
-          name={REGISTRATION_INPUT.phone.name}
-          label={REGISTRATION_INPUT.phone.label}
-          register={register(REGISTRATION_INPUT.phone.name)}
-        />
-      </li>
-      <li>
-        <InputBox
-          type='email'
-          name={REGISTRATION_INPUT.email.name}
-          label={REGISTRATION_INPUT.email.label}
-          register={register(REGISTRATION_INPUT.email.name)}
-        />
-      </li>
-    </>
-  );
-};
+export const ThirdStep = () => (
+  <>
+    <li>
+      <InputBox
+        type='text'
+        name={REGISTRATION_INPUT.phone.name}
+        label={REGISTRATION_INPUT.phone.label}
+        registerOptions={{ required: true }}
+      />
+    </li>
+    <li>
+      <InputBox
+        type='email'
+        name={REGISTRATION_INPUT.email.name}
+        label={REGISTRATION_INPUT.email.label}
+        registerOptions={{ required: true }}
+      />
+    </li>
+  </>
+);
