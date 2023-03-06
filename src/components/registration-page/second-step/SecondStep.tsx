de50e36a658cb9bad5da8dbd0ea67ad1777065aa
Anton Kachan <aa.kvchvn@gmail.com>
2023-03-07 01:00:15 +0300

    fix: fix props in SecondStep

--- src/components/registration-page/second-step/SecondStep.tsx
+++ src/components/registration-page/second-step/SecondStep.tsx
@@ -1,30 +1,25 @@
 import React from 'react';
-import { useFormContext } from 'react-hook-form';
 
 import { REGISTRATION_INPUT } from '../../../constants';
 import { InputBox } from '../../common/input-box';
 
-export const SecondStep = () => {
-  const { register } = useFormContext();
-
-  return (
-    <>
-      <li>
-        <InputBox
-          type='text'
-          name={REGISTRATION_INPUT.firstName.name}
-          label={REGISTRATION_INPUT.firstName.label}
-          register={register(REGISTRATION_INPUT.firstName.name, { required: true })}
-        />
-      </li>
-      <li>
-        <InputBox
-          type='text'
-          name={REGISTRATION_INPUT.lastName.name}
-          label={REGISTRATION_INPUT.lastName.label}
-          register={register(REGISTRATION_INPUT.lastName.name, { required: true })}
-        />
-      </li>
-    </>
-  );
-};
+export const SecondStep = () => (
+  <>
+    <li>
+      <InputBox
+        type='text'
+        name={REGISTRATION_INPUT.firstName.name}
+        label={REGISTRATION_INPUT.firstName.label}
+        registerOptions={{ required: true }}
+      />
+    </li>
+    <li>
+      <InputBox
+        type='text'
+        name={REGISTRATION_INPUT.lastName.name}
+        label={REGISTRATION_INPUT.lastName.label}
+        registerOptions={{ required: true }}
+      />
+    </li>
+  </>
+);
