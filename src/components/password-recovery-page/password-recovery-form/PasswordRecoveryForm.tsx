8480658666d861e6f8a8825f73a7ea7224c6a7c7
Anton Kachan <aa.kvchvn@gmail.com>
2023-03-09 17:40:24 +0300

    feat: connect to useForm and make required layout in PasswordRecoveryForm

--- src/components/password-recovery-page/password-recovery-form/PasswordRecoveryForm.tsx
+++ src/components/password-recovery-page/password-recovery-form/PasswordRecoveryForm.tsx
@@ -1,35 +1,57 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
-import { PASSWORD_RECOVERY_INPUT, ROUTES } from '../../../constants';
+import { PASSWORD_RECOVERY_INPUT, ROUTES, VALIDATION_ERROR } from '../../../constants';
+import { check } from '../../../helpers';
+import { ForgotPassRequestBody } from '../../../types';
 import { InputBox } from '../../common/input-box';
 
 import styles from './PasswordRecoveryForm.module.scss';
 
 export const PasswordRecoveryForm = () => {
-  const handleClick = () => alert('Восстановление');
+  const {
+    handleSubmit: handleSubmitWrapper,
+    control,
+    formState: { isValid: isFormValid, submitCount },
+  } = useForm<ForgotPassRequestBody>();
+
+  const handleSubmit = handleSubmitWrapper((formValues) => {
+    console.log(formValues);
+  });
 
   return (
     <>
-      <div />
-      <h4>Восстановление пароля</h4>
-      <form autoComplete='off'>
-        <InputBox
-          type='email'
-          name={PASSWORD_RECOVERY_INPUT.email.name}
-          label={PASSWORD_RECOVERY_INPUT.email.label}
-          initialAssistiveText={PASSWORD_RECOVERY_INPUT.email.assistiveText}
-        />
-      </form>
-      <div className={styles['submit-box']}>
-        <button type='button' onClick={handleClick}>
-          Восстановить
-        </button>
-        <p>
-          Нет учетной записи?
-          <Link to={ROUTES.registration}>Регистрация</Link>
-        </p>
+      <div className={styles['link-box']}>
+        <Link to={ROUTES.auth}>Вход в личный кабинет</Link>
       </div>
+      <h4 className={styles.heading}>Восстановление пароля</h4>
+      <form autoComplete='off' onSubmit={handleSubmit} className={styles.form}>
+        <div>
+          <InputBox
+            type='email'
+            name={PASSWORD_RECOVERY_INPUT.email.name}
+            label={PASSWORD_RECOVERY_INPUT.email.label}
+            control={control}
+            validationRules={{
+              validate: (val) => check(val).hasValidEmail || VALIDATION_ERROR.invalidEmail,
+              required: VALIDATION_ERROR.requiredField,
+            }}
+          />
+          <p className={styles['assistive-text']}>
+            На это email будет отправлено письмо с инструкциями по восстановлению пароля
+          </p>
+        </div>
+        <div className={styles['submit-box']}>
+          <button type='submit' disabled={!isFormValid && submitCount > 0}>
+            Восстановить
+          </button>
+          <p>
+            Нет учетной записи?
+            <Link to={ROUTES.registration}>Регистрация</Link>
+          </p>
+        </div>
+      </form>
     </>
   );
 };
