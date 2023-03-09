4b87d55cf9d2282f4c58f31d087404960871d26b
Anton Kachan <aa.kvchvn@gmail.com>
2023-03-09 14:35:33 +0300

    feat: create helper function for checking type of request error

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -5,6 +5,8 @@ export {
   getFromLocalStorage,
   clearLocalStorage,
   removeFromLocalStorage,
+  getRequestErrorStatusCode,
 } from './common';
 export { formatDate, setButtonReserveText, setBookAuthorsAndYearText, calculateBooksByCategories } from './books';
 export { validateLogin, validatePassword, check, validatePhone, validateEmail } from './validation';
+export { isRequestError } from './typesDefinition';
