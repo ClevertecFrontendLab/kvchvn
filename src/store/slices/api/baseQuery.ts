4e611a11c82c35b1677d5c8ff45ba4443bc25424
Anton Kachan <aa.kvchvn@gmail.com>
2023-03-09 15:39:23 +0300

    feat: define prepareHeaders in baseQuery

--- src/store/slices/api/baseQuery.ts
+++ src/store/slices/api/baseQuery.ts
@@ -7,10 +7,18 @@ import {
 } from '@reduxjs/toolkit/dist/query/react';
 
 import { BASE_API_URL } from '../../../constants';
+import { getFromLocalStorage } from '../../../helpers';
 import { RequestError } from '../../../types/api';
 
 export const baseQuery = fetchBaseQuery({
   baseUrl: `${BASE_API_URL}/api/`,
+  prepareHeaders: (headers) => {
+    const token = getFromLocalStorage('jwt');
+
+    if (token) {
+      headers.set('Authorization', `Bearer ${token}`);
+    }
+  },
 }) as BaseQueryFn<
   string | FetchArgs,
   unknown,
