8b489f5f22840e4fdc8130605d1d3ef46b6f78c3
Anton Kachan <aa.kvchvn@gmail.com>
2023-03-10 18:27:17 +0300

    feat: create reusable component AuthModal

---
+++ src/components/global/auth-modal/AuthModal.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './AuthModal.module.scss';
+
+interface AuthModalProps {
+  children: React.ReactNode;
+}
+
+export const AuthModal = ({ children }: AuthModalProps) => <section className={styles.section}>{children}</section>;
