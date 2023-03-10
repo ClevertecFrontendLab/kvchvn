c6091eab7c7c14f57783423eb6005649a78e3739
Anton Kachan <aa.kvchvn@gmail.com>
2023-03-10 21:39:11 +0300

    feat: implement Layouts for Private and Public routes

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,6 +3,8 @@ import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import { AuthLayout } from '../components/global/auth-layout';
 import { Layout } from '../components/global/layout';
+import { PrivateRoutesLayout } from '../components/global/private-routes-layout';
+import { PublicRoutesLayout } from '../components/global/public-routes-layout';
 import { ScrollToTop } from '../components/global/scroll-to-top';
 import { ROUTES } from '../constants';
 import { AuthPage } from '../pages/auth';
@@ -18,17 +20,21 @@ export const Router = () => (
     <ScrollToTop>
       <Routes>
         <Route path={ROUTES.main} element={<Layout />}>
-          <Route index={true} element={<Navigate to={ROUTES.books.all} />} />
-          <Route path={ROUTES.books.base} element={<Navigate to={ROUTES.books.all} />} />
-          <Route path={ROUTES.books.category} element={<MainPage />} />
-          <Route path={ROUTES.books.specificBook} element={<BookPage />} />
-          <Route path={ROUTES.terms} element={<TermsPage view='terms' />} />
-          <Route path={ROUTES.contract} element={<TermsPage view='contract' />} />
+          <Route path={ROUTES.main} element={<PrivateRoutesLayout />}>
+            <Route index={true} element={<Navigate to={ROUTES.books.all} />} />
+            <Route path={ROUTES.books.base} element={<Navigate to={ROUTES.books.all} />} />
+            <Route path={ROUTES.books.category} element={<MainPage />} />
+            <Route path={ROUTES.books.specificBook} element={<BookPage />} />
+            <Route path={ROUTES.terms} element={<TermsPage view='terms' />} />
+            <Route path={ROUTES.contract} element={<TermsPage view='contract' />} />
+          </Route>
         </Route>
         <Route path={ROUTES.main} element={<AuthLayout />}>
-          <Route path={ROUTES.auth} element={<AuthPage />} />
-          <Route path={ROUTES.registration} element={<RegistrationPage />} />
-          <Route path={ROUTES.passwordRecovery} element={<PasswordRecoveryPage />} />
+          <Route path={ROUTES.main} element={<PublicRoutesLayout />}>
+            <Route path={ROUTES.auth} element={<AuthPage />} />
+            <Route path={ROUTES.registration} element={<RegistrationPage />} />
+            <Route path={ROUTES.passwordRecovery} element={<PasswordRecoveryPage />} />
+          </Route>
         </Route>
         <Route path={ROUTES.notFound} element={<NotFoundPage />} />
       </Routes>
