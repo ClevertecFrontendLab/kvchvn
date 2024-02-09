ebfa50a3e8974a29c29817358f7d79d94a3191fe
Anton Kachan <aa.kvchvn@gmail.com>
2024-02-09 14:29:31 +0300

    feat(Sidebar): implement base layout of Sidebar

---
+++ src/components/app/sidebar/sidebar.tsx
@@ -0,0 +1,36 @@
+import { Layout } from 'antd';
+import React, { createContext, useState } from 'react';
+import { CollapseButton } from './collapse-button';
+import { ExitButton } from './exit-button/exit-button';
+import { Logo } from './logo';
+import { Navigation } from './navigation';
+import s from './sidebar.module.css';
+
+type Context = {
+    isCollapsed: boolean;
+};
+
+export const SidebarContext = createContext<Context>({ isCollapsed: false });
+
+export const Sidebar: React.FC = () => {
+    const [isCollapsed, setIsCollapsed] = useState(false);
+
+    const toggleCollapsing = () => {
+        setIsCollapsed((prevState) => !prevState);
+    };
+
+    return (
+        <SidebarContext.Provider value={{ isCollapsed }}>
+            <Layout.Sider
+                collapsible={true}
+                collapsed={isCollapsed}
+                trigger={null}
+                width={208}
+                collapsedWidth={64}
+                className={s.sidebar}
+            >
+                <Navigation />
+            </Layout.Sider>
+        </SidebarContext.Provider>
+    );
+};
