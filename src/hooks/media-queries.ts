62235ad8e22c9f441ef6257eeb317768dd26fad0
Anton Kachan <aa.kvchvn@gmail.com>
2024-02-13 00:41:25 +0300

    feat(useMedia): add more resolutions

--- src/hooks/media-queries.ts
+++ src/hooks/media-queries.ts
@@ -4,6 +4,10 @@ const SIZES = {
     xl: 1280,
     lg: 1024,
     md: 835,
+    // like semi-medium
+    smd: 768,
+    sm: 600,
+    xs: 480,
 } as const;
 
 export const useMedia = (size: keyof typeof SIZES) => useMediaQuery({ maxWidth: SIZES[size] });
