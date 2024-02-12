f4be2a79970020dc802915a86c07aceb45160ab5
Anton Kachan <aa.kvchvn@gmail.com>
2024-02-13 00:53:50 +0300

    feat(CollapseButton): add data-test-id attribute

--- src/components/app/sidebar/collapse-button/collapse-button.tsx
+++ src/components/app/sidebar/collapse-button/collapse-button.tsx
@@ -1,4 +1,5 @@
 import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
+import { useMedia } from '@hooks/media-queries';
 import React, { useContext } from 'react';
 import { SidebarContext } from '../sidebar';
 import s from './collapse-button.module.css';
@@ -10,9 +11,15 @@ type Props = {
 export const CollapseButton: React.FC<Props> = ({ toggleSidebarCollapsing }) => {
     const { isCollapsed } = useContext(SidebarContext);
 
+    const isSmScreen = useMedia('sm');
+
     return (
         <div className={s.container}>
-            <button onClick={toggleSidebarCollapsing} className={s.button}>
+            <button
+                onClick={toggleSidebarCollapsing}
+                className={s.button}
+                data-test-id={isSmScreen ? 'sider-switch-mobile' : 'sider-switch'}
+            >
                 {isCollapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
             </button>
         </div>
