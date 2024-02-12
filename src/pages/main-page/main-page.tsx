31d2a5db655787a3828beaa7c3b8ea5558f1e7a8
Anton Kachan <aa.kvchvn@gmail.com>
2024-02-12 14:48:04 +0300

    feat(MainPage): insert all inner components; apply styles

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,5 +1,22 @@
+import { Header } from '@components/app/header';
+import { Cards } from '@components/main-page/cards';
+import { Description } from '@components/main-page/description';
+import { Footer } from '@components/main-page/footer';
+import { Slogan } from '@components/main-page/slogan';
+import { Layout } from 'antd';
 import React from 'react';
+import s from './main-page.module.css';
 
 export const MainPage: React.FC = () => {
-    return <p>Main Page</p>;
+    return (
+        <>
+            <Header />
+            <Layout.Content className={s.container}>
+                <Description />
+                <Slogan />
+                <Cards />
+            </Layout.Content>
+            <Footer />
+        </>
+    );
 };
