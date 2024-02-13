854388100aea8cd8da2350ece711acec56d8a366
Anton Kachan <aa.kvchvn@gmail.com>
2024-02-13 12:00:17 +0300

    fix(Logo): prevent logo changing on small screens

--- src/components/app/sidebar/logo/logo.tsx
+++ src/components/app/sidebar/logo/logo.tsx
@@ -1,3 +1,4 @@
+import { useMedia } from '@hooks/media-queries';
 import { Icon } from '@ui/icon';
 import classNames from 'classnames';
 import React, { useContext } from 'react';
@@ -7,10 +8,12 @@ import s from './logo.module.css';
 export const Logo: React.FC = () => {
     const { isCollapsed } = useContext(SidebarContext);
 
+    const isSmScreen = useMedia('sm');
+
     return (
         <Icon
             file='logo'
-            id={isCollapsed ? 'logo-short' : 'logo-long'}
+            id={isCollapsed && !isSmScreen ? 'logo-short' : 'logo-long'}
             className={classNames(s.logo, { [s.collapsed]: isCollapsed })}
         />
     );
