84868d6911b32cedc40e9b321af046deaf8c8110
Anton Kachan <aa.kvchvn@gmail.com>
2024-02-13 12:26:38 +0300

    refactor(Header): remove useless markup

--- src/components/app/header/header.tsx
+++ src/components/app/header/header.tsx
@@ -1,13 +1,12 @@
 import { SettingOutlined } from '@ant-design/icons';
 import { useMedia } from '@hooks/media-queries';
-import { Breadcrumb, Button, Col, Layout } from 'antd';
+import { Breadcrumb, Button, Layout } from 'antd';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import s from './header.module.css';
 
 export const Header: React.FC = () => {
     const isXlScreen = useMedia('xl');
-    const isLgScreen = useMedia('lg');
     const isSmdScreen = useMedia('smd');
 
     return (
@@ -18,22 +17,19 @@ export const Header: React.FC = () => {
                 </Breadcrumb>
                 <section className={s['columns-container']}>
                     <h1 className={s.title}>
-                        Приветствуем тебя в CleverFit — приложении,
-                        {isLgScreen && <br />}
-                        которое поможет тебе добиться своей мечты!
+                        Приветствуем тебя в CleverFit — приложении, которое поможет тебе добиться
+                        своей мечты!
                     </h1>
-                    <Col flex='none'>
-                        <Link to='/settings'>
-                            <Button
-                                type={isSmdScreen ? 'default' : 'text'}
-                                shape={isSmdScreen ? 'circle' : 'default'}
-                                icon={isXlScreen && !isSmdScreen ? null : <SettingOutlined />}
-                                className={s.button}
-                            >
-                                {isSmdScreen ? null : 'Настройки'}
-                            </Button>
-                        </Link>
-                    </Col>
+                    <Link to='/settings'>
+                        <Button
+                            type={isSmdScreen ? 'default' : 'text'}
+                            shape={isSmdScreen ? 'circle' : 'default'}
+                            icon={isXlScreen && !isSmdScreen ? null : <SettingOutlined />}
+                            className={s.button}
+                        >
+                            {isSmdScreen ? null : 'Настройки'}
+                        </Button>
+                    </Link>
                 </section>
             </div>
         </Layout.Header>
