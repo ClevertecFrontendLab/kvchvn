aadd9a4cf0afe4ee244fd1be2346c025cefdcd3d
Anton Kachan <aa.kvchvn@gmail.com>
2024-02-15 14:19:04 +0300

    feat(store): initialize redux-history and connect it to store

---
+++ src/redux/store.ts
@@ -0,0 +1,21 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { createBrowserHistory } from 'history';
+import { createReduxHistoryContext } from 'redux-first-history';
+
+const { createReduxHistory, routerReducer, routerMiddleware } = createReduxHistoryContext({
+    history: createBrowserHistory(),
+});
+
+export const store = configureStore({
+    reducer: combineReducers({
+        router: routerReducer,
+    }),
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(routerMiddleware,
+        ),
+});
+
+export const history = createReduxHistory(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
